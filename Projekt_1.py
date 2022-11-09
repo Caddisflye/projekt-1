36737f9a10a596722ee08ae35815c4581128b54f
Caddisflye <jiristeif@seznam.cz>
2022-11-09 18:33:20 +0100

    Test of commit

--- Projekt_1.py
+++ Projekt_1.py
@@ -1,1 +1,25 @@
-print("Hello world")
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
+author: Jiri Steif
+email: jiristeif@seznam.cz
+discord: Caddi#3130
+"""
+
+from task_template import TEXTS as texts
+
+users = {
+    "bob": "123",
+    "ann": "pass123",
+    "mike": "password123",
+    "liz": "pass123"
+}
+
+user = input("Enter your username: ").lower()
+password = input("Enter your password: ")
+
+if user in users.keys() and password == users.get(user):
+    print(f"Login successful, welcome {user}!")
+else:
+    print("Unknown user")
+    exit()
+
